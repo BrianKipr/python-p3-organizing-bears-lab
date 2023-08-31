ffd09dd4c4a31496cb5e14c5fcd513b25dff4d0f
Briankipr <brian.kiprono1@student.moringaschool.com>
2023-08-31 15:06:35 +0300

    assignmant done

--- lib/sql_queries.py
+++ lib/sql_queries.py
@@ -1,18 +1,38 @@
+# SQL query to select all female bears and return their name and age
 select_all_female_bears_return_name_and_age = """
-    Write your SQL query here
+SELECT name, age
+FROM bears
+WHERE gender = 'female';
 """
 
+# SQL query to select all bears' names and order them in alphabetical order
 select_all_bears_names_and_orders_in_alphabetical_order = """
-    Write your SQL query here
+SELECT name
+FROM bears
+ORDER BY name ASC;
 """
 
+# SQL query to select all bears' names and ages that are alive and order them from youngest to oldest
 select_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest = """
-    Write your SQL query here
+SELECT name, age
+FROM bears
+WHERE status = 'alive'
+ORDER BY age ASC;
 """
 
+# SQL query to select the oldest bear and return their name and age
 select_oldest_bear_and_returns_name_and_age = """
-    Write your SQL query here
+SELECT name, age
+FROM bears
+ORDER BY age DESC
+LIMIT 1;
 """
+
+# SQL query to select the youngest bear and return their name and age
 select_youngest_bear_and_returns_name_and_age = """
-    Write your SQL query here
-"""
+SELECT name, age
+FROM bears
+ORDER BY age ASC
+LIMIT 1;
+"""*
+ pl
