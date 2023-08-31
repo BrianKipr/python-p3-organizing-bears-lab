5e9cb4b3a89c27bf3a20442cdb94764435511057
Briankipr <brian.kiprono1@student.moringaschool.com>
2023-08-31 15:14:10 +0300

    assignment done

--- lib/sql_queries.py
+++ lib/sql_queries.py
@@ -1,38 +1,41 @@
-# SQL query to select all female bears and return their name and age
 select_all_female_bears_return_name_and_age = """
-SELECT name, age
-FROM bears
-WHERE gender = 'female';
+    SELECT
+        bears.name,
+        bears.age
+    FROM bears
+    WHERE sex='F';
 """
 
-# SQL query to select all bears' names and order them in alphabetical order
 select_all_bears_names_and_orders_in_alphabetical_order = """
-SELECT name
-FROM bears
-ORDER BY name ASC;
+    SELECT
+        bears.name
+    FROM bears
+    ORDER BY bears.name ASC;
 """
 
-# SQL query to select all bears' names and ages that are alive and order them from youngest to oldest
 select_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest = """
-SELECT name, age
-FROM bears
-WHERE status = 'alive'
-ORDER BY age ASC;
+    SELECT
+      bears.name,
+      bears.age
+    FROM bears
+    WHERE alive=1
+    ORDER BY bears.age ASC;
 """
 
-# SQL query to select the oldest bear and return their name and age
 select_oldest_bear_and_returns_name_and_age = """
-SELECT name, age
-FROM bears
-ORDER BY age DESC
-LIMIT 1;
+    SELECT
+      bears.name,
+      bears.age
+    FROM bears
+    ORDER BY bears.age DESC
+    LIMIT 1;
 """
 
-# SQL query to select the youngest bear and return their name and age
 select_youngest_bear_and_returns_name_and_age = """
-SELECT name, age
-FROM bears
-ORDER BY age ASC
-LIMIT 1;
-"""*
- pl
+    SELECT
+      bears.name,
+      bears.age
+    FROM bears
+    ORDER BY bears.age ASC
+    LIMIT 1;
+"""
